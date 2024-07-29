c9e38734d5fe11b6d3bc933e0ca8c601239932d3
Shilpa Gopal <shlpgopal@gmail.com>
2024-07-29 18:05:44 -0400

    add users to db

--- NotesWebsite/website/auth.py
+++ NotesWebsite/website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template,request,flash
+from flask import Blueprint, render_template,request,flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth',__name__)
 
@@ -18,19 +22,24 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstname = request.form.get('firstname')
+        first_name = request.form.get('firstname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash("Email must be greater than 4 characters",category='error')
-        elif len(firstname) < 2:
+        elif len(first_name) < 2:
             flash("Firstname must be greater than 2 characters",category='error')
         elif password1 != password2:
             flash("Passwords don't match",category='error')
         elif len(password1) < 7:
             flash("Password must be at least 7 characters",category='error')
         else:
-            flash("Success",category='success')
+            new_user = User(email=email,firstname=first_name,password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account Create",category='success')
+            return redirect(url_for('views.home'))
+            
         
     return  render_template("sign_up.html")
