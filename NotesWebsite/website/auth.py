063f306d78bae5aab2b270c9f162fe29e51312ce
Shilpa Gopal <shlpgopal@gmail.com>
2024-07-29 18:10:54 -0400

    login and signup

--- NotesWebsite/website/auth.py
+++ NotesWebsite/website/auth.py
@@ -11,6 +11,19 @@ auth = Blueprint('auth',__name__)
 def login():
     # data = request.form
     # print(data)
+    if request.methods==['GET','POST']:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        
+        if user: 
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully",category='success')
+            else:
+                flash("Incorrect password, try again",category='error')
+        else:
+            flash("Email does not exist",category='error')
+            
     return render_template("login.html", text='Testing')
 
 
@@ -26,7 +39,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists",category='error')
+        elif len(email) < 4:
             flash("Email must be greater than 4 characters",category='error')
         elif len(first_name) < 2:
             flash("Firstname must be greater than 2 characters",category='error')
